155193e8867fb3167213546e8adc4b962908e8c9
akimov.infotech <akimov.infotech@gmail.com>
2019-04-21 23:05:50 +0600

    added custom directive - CounterDirective,  CartSummaryComponent

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,20 +1,59 @@
 import { Component } from "@angular/core";
 import { Product } from "../model/product.model";
 import { ProductRepository } from "../model/product.repository";
+import { Cart } from '../model/cart.model';
 
 @Component({
     selector: "store",
     templateUrl: "store.component.html"
 })
 export class StoreComponent {
+    public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
+    // public valueType;
 
-    constructor(private repository: ProductRepository) { }
+    constructor(private repository: ProductRepository, private cart: Cart) { }
 
     get products(): Product[] {
-        return this.repository.getProducts();
+        // console.log('produts getter called')        
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
-    
+
     get categories(): string[] {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number) {
+        //console.log('--------');// dosn't work
+        // this.valueType = Number(newSize);
+        // console.log(this.valueType);
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    // get pageNumbers(): number[] {
+    //     return Array(Math.ceil(this.repository
+    //         .getProducts(this.selectedCategory).length / this.productsPerPage))
+    //         .fill(0).map((x, i) => i + 1);
+    // }
+
+    get pageCount(): number {
+        return Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+
+    addProductToCart(product: Product) {
+        this.cart.addLine(product);
     }
 }
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from "@angular/forms";
 
 import { StoreComponent } from "./store.component";
 import { ModelModule } from '../model/model.module';
+import { CounterDirective } from './counter.directive';
+import { CartSummaryComponent } from './cartSummary.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
-    declarations: [StoreComponent],
+    declarations: [StoreComponent, CounterDirective, CartSummaryComponent],
     exports: [StoreComponent]
 })
 export class StoreModule { }
