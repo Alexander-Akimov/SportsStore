dd9ab5a0b7c630b4a1620675cb51565fd12a56cf
Alexander Akimov <akimov.infotech@gmail.com>
2019-04-25 13:23:20 +0600

    using RestDataSource

--- src/app/model/cart.model.ts
+++ src/app/model/cart.model.ts
@@ -6,7 +6,7 @@ export class Cart {
     public lines: CartLine[] = [];
     public itemCount: number = 0;
     public cartPrice: number = 0;
-    
+
     addLine(product: Product, quantity: number = 1) {
         let line = this.lines.find(line => line.product.id == product.id);
         if (line != undefined) {
@@ -16,6 +16,7 @@ export class Cart {
         }
         this.recalculate();
     }
+
     updateQuantity(product: Product, quantity: number) {
         let line = this.lines.find(line => line.product.id == product.id);
         if (line != undefined) {
@@ -23,16 +24,19 @@ export class Cart {
         }
         this.recalculate();
     }
+
     removeLine(id: number) {
         let index = this.lines.findIndex(line => line.product.id == id);
         this.lines.splice(index, 1);
         this.recalculate();
     }
+
     clear() {
         this.lines = [];
         this.itemCount = 0;
         this.cartPrice = 0;
     }
+    
     private recalculate() {
         this.itemCount = 0;
         this.cartPrice = 0;
@@ -44,8 +48,8 @@ export class Cart {
 }
 
 export class CartLine {
-    constructor(public product: Product,
-        public quantity: number) { }
+    constructor(public product: Product, public quantity: number) { }
+
     get lineTotal() {
         return this.quantity * this.product.price;
     }
