3fda7a397c693b68ecf8c931a3aee8868d2a62d5
Alexander Akimov <akimov.infotech@gmail.com>
2019-04-27 14:09:25 +0600

    change port to 5000, set routes for json-server

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+import { Injectable, Inject } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Product } from "./product.model";
@@ -6,21 +6,22 @@ import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 
 const PROTOCOL = "http";
-const PORT = 3500;
+const PORT = 5000;
 
 @Injectable()
 export class RestDataSource {
     baseUrl: string;
 
-    constructor(private http: HttpClient) {
-        this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+         this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+        //this.baseUrl = baseUrl;//for https
     }
 
     getProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.baseUrl + "products");
+        return this.http.get<Product[]>(this.baseUrl + "api/products");
     }
     
     saveOrder(order: Order): Observable<Order> {
-        return this.http.post<Order>(this.baseUrl + "orders", order);
+        return this.http.post<Order>(this.baseUrl + "api/orders", order);
     }
 }
