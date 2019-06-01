38729fb806a8a6bae1d6981ec25e41119820a0dc
Alexander Akimov <akimov.infotech@gmail.com>
2019-06-01 23:38:31 +0600

    PWA features are added

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,10 +1,21 @@
 import { Component } from "@angular/core";
 import { Cart } from '../model/cart.model';
+import { ConnectionService } from '../model/connection.service';
 
 @Component({
     // template: `<div><h3 class="bg-info p-1 text-white">Cart Detail Component</h3></div>`,
     templateUrl: "cartDetail.component.html"
 })
 export class CartDetailComponent {
-    constructor(public cart: Cart) { }
+    public connected: boolean = true;
+
+
+    constructor(public cart: Cart, private connection: ConnectionService) {
+        this.connected = this.connection.connected;
+        connection.Changes.subscribe((state) => this.connected = state);
+    }
+
+    get checkoutEnabled(): boolean {
+        return this.cart.lines.length == 0 || !this.connected;
+    }
 }
